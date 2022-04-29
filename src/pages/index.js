a7489987ebf46cfab654b4d3836a8cdaab5519fc
hattuk <98957234+hattuk@users.noreply.github.com>
2022-04-29 23:18:34 +0300

    Update index.js
    h1 h2 denemeleri

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,8 @@
 import React from "react"
 
-const IndexPage = () => (<div><h1>Ilk web siteme hosgeldin !</h1></div>)
+const IndexPage = () => (<div>
+    <h1>Ilk web siteme hosgeldin !</h1>
+    <h2>Ilk web siteme hosgeldin !</h2>
+    <h1>Ilk web siteme hosgeldiniz !</h1>
+    </div>)
 export default IndexPage
