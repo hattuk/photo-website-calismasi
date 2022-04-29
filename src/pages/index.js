50bea0b4e47208bba1d9b3df99321c1c05533856
hattuk <98957234+hattuk@users.noreply.github.com>
2022-04-29 23:12:27 +0300

    Update index.js
    ana sayfa giris mesajim degisti

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 
-const IndexPage = () => (<div><h1>Merhaba</h1></div>)
+const IndexPage = () => (<div><h1>Ilk web siteme hosgeldin !</h1></div>)
 export default IndexPage
