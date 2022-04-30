75c02e450973af32c6c580da4cf5c3f0a92f65ed
hattuk <98957234+hattuk@users.noreply.github.com>
2022-04-30 11:50:56 +0300

    Update index.js
    zara

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from "react"
 
 const IndexPage = () => (<div>
-    <h1>Ilk web siteme hosgeldin !</h1>
-    <h2>Ilk web siteme hosgeldin !</h2>
-    <h1>Ilk web siteme hosgeldiniz !</h1>
+    <h1>Ilk web siteme hosgeldin Zara!</h1>
+
     </div>)
 export default IndexPage
